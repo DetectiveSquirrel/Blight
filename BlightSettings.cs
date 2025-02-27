c5f03abcd8e334f944bb7690ea0167d893498dd1
DetectiveSquirrel <2137769+DetectiveSquirrel@users.noreply.github.com>
2025-02-28 00:51:37 +1300

    cleanup + map drawing additions

--- BlightSettings.cs
+++ BlightSettings.cs
@@ -1,38 +1,92 @@
-﻿using ExileCore.Shared.Interfaces;
+﻿using ExileCore.Shared.Attributes;
+using ExileCore.Shared.Interfaces;
 using ExileCore.Shared.Nodes;
 using SharpDX;
 
-namespace Blight
+namespace Blight;
+
+public static class ColorExtensions
 {
-    public class BlightSettings : ISettings
+    public static Color ToSharpDx(this System.Drawing.Color color)
     {
-        //Mandatory setting to allow enabling/disabling your plugin
-        public ToggleNode Enable { get; set; } = new ToggleNode(false);
-        public ColorNode BlightColorMap { get; set; } = new Color(0, 200, 0, 150);
-        public RangeNode<int> BlightWidthMap { get; set; } = new RangeNode<int>(3, 1, 100);
-        public ColorNode BlightColorWorld { get; set; } = new Color(0, 200, 0, 150);
-        public RangeNode<int> BlightWidthWorld { get; set; } = new RangeNode<int>(8, 1, 100);
-        public RangeNode<int> MaxWorldDrawDistance { get; set; } = new RangeNode<int>(160, 1, 600);
-        public ToggleNode DrawMap { get; set; } = new ToggleNode(true);
-        public ToggleNode DrawWorld { get; set; } = new ToggleNode(false);
-        public ToggleNode DisableDrawOnLeftOrRightPanelsOpen { get; set; } = new ToggleNode(false);
-        public ToggleNode IgnoreFullscreenPanels { get; set; } = new ToggleNode(false);
-        public ToggleNode IgnoreLargePanels { get; set; } = new ToggleNode(false);
-
-
-        public RangeNode<float> CircleThickness { get; set; } = new RangeNode<float>(3f, 1f, 15f);
-        public ColorNode FireTowerColor { get; set; } = new ColorNode(Color.Red);
-        public ColorNode ColdTowerColor { get; set; } = new ColorNode(Color.SkyBlue);
-        public ColorNode LightningTowerColor { get; set; } = new ColorNode(Color.LightYellow);
-        public ColorNode PhysicalTowerColor { get; set; } = new ColorNode(Color.Brown);
-        public ColorNode MinionTowerColor { get; set; } = new ColorNode(Color.Purple);
-        public ColorNode BuffTowerColor { get; set; } = new ColorNode(Color.LimeGreen);
-
-        public ToggleNode FireTowerGround { get; set; } = new ToggleNode(true);
-        public ToggleNode ColdTowerGround { get; set; } = new ToggleNode(true);
-        public ToggleNode LightningTowerGround { get; set; } = new ToggleNode(true);
-        public ToggleNode PhysicalTowerGround { get; set; } = new ToggleNode(true);
-        public ToggleNode MinionTowerGround { get; set; } = new ToggleNode(true);
-        public ToggleNode BuffTowerGround { get; set; } = new ToggleNode(true);
+        return new Color(color.R, color.G, color.B, color.A);
     }
+}
+
+public class BlightSettings : ISettings
+{
+    public ToggleNode Enable { get; set; } = new(false);
+    public ToggleNode DisableDrawOnLeftOrRightPanelsOpen { get; set; } = new(false);
+    public ToggleNode IgnoreFullscreenPanels { get; set; } = new(false);
+    public ToggleNode IgnoreLargePanels { get; set; } = new(false);
+
+    public Pathway Pathways { get; set; } = new();
+    public TowerList Towers { get; set; } = new();
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class Pathway
+{
+    public ColorNode MapColor { get; set; } = new Color(255,255,255, 111);
+    public ColorNode WorldColor { get; set; } = new Color(194,200, 0, 57);
+    public RangeNode<int> MapLineWidth { get; set; } = new(3, 1, 100);
+    public RangeNode<int> WorldLineWidth { get; set; } = new(8, 1, 100);
+    public ToggleNode DrawMap { get; set; } = new(true);
+    public ToggleNode DrawWorld { get; set; } = new(true);
+}
+
+[Submenu(CollapsedByDefault = false)]
+public class TowerList
+{
+    public TowerSettings FireTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.Red),
+        DrawMap = new ToggleNode(false)
+    };
+
+    public TowerSettings ColdTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.SkyBlue),
+        DrawMap = new ToggleNode(false)
+    };
+
+    public TowerSettings LightningTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.LightYellow),
+        DrawMap = new ToggleNode(false)
+    };
+
+    public TowerSettings PhysicalTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.Brown),
+        DrawMap = new ToggleNode(false)
+    };
+
+    public TowerSettings MinionTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.Purple),
+        DrawMap = new ToggleNode(false)
+    };
+
+    public TowerSettings BuffTower { get; set; } = new()
+    {
+        DrawGround = new ToggleNode(true),
+        Color = new ColorNode(Color.LimeGreen),
+        DrawMap = new ToggleNode(false)
+    };
+}
+
+
+[Submenu(CollapsedByDefault = false)]
+public class TowerSettings
+{
+    public ToggleNode DrawGround { get; set; } = new(true);
+    public ToggleNode DrawMap { get; set; } = new(false);
+    public RangeNode<float> CircleThickness { get; set; } = new(3f, 1f, 15f);
+    public ColorNode Color { get; set; } = System.Drawing.Color.FromArgb(255, 255, 255, 255).ToSharpDx();
 }
--- Main.cs
+++ Main.cs
@@ -1,103 +1,62 @@
-﻿using ExileCore;
+﻿using System.Collections.Generic;
+using System.Linq;
+using ExileCore;
 using ExileCore.PoEMemory;
 using ExileCore.PoEMemory.Components;
-using ExileCore.PoEMemory.Elements;
 using ExileCore.PoEMemory.MemoryObjects;
 using ExileCore.Shared.Helpers;
 using SharpDX;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using Vector2 = System.Numerics.Vector2;
 using Vector3 = System.Numerics.Vector3;
 
 namespace Blight;
 
 public class Main : BaseSettingsPlugin<BlightSettings>
 {
-    private const double CameraAngle = 38.7 * Math.PI / 180;
+    public Entity AreaPumpEntity { get; private set; }
+    public bool AreaPumpStopped { get; private set; }
+    public List<Entity> BlightPathways { get; private set; } = new();
+    public bool LargeMapOpen { get; private set; }
 
-    private const float GridToWorldMultiplier = 250 / 23f;
-    private static readonly float CameraAngleCos = (float)Math.Cos(CameraAngle);
-    private static readonly float CameraAngleSin = (float)Math.Sin(CameraAngle);
+    public List<Entity> PathwayEntityList { get; } = new();
 
-    private bool _largeMapOpen;
-    private Vector2 _mapCenter;
-    private double _mapScale;
-    private Vector2 _playerGridPos;
-    private float _playerZ;
-    private Entity areaPumpEntity;
-    private bool areaPumpStopped;
-
-    public List<Entity> BlightEntities = [];
-    public List<Entity> drawList = [];
-    private List<Entity> towerList = [];
-
-    public Vector3 PlayerPos { get; set; }
-    public IngameData IngameData { get; set; }
-    private Camera Camera => GameController.Game.IngameState.Camera;
+    public List<Entity> TowerEntityList { get; } = new();
 
     public override bool Initialise()
     {
-        BlightEntities = [];
-        towerList = [];
-        areaPumpEntity = null;
-        areaPumpStopped = false;
+        PathwayEntityList.Clear();
+        TowerEntityList.Clear();
+        AreaPumpEntity = null;
+        AreaPumpStopped = false;
         return true;
     }
 
     public override void AreaChange(AreaInstance area)
     {
-        BlightEntities = [];
-        towerList = [];
-        areaPumpEntity = null;
-        areaPumpStopped = false;
+        PathwayEntityList.Clear();
+        TowerEntityList.Clear();
+        AreaPumpEntity = null;
+        AreaPumpStopped = false;
     }
 
     public override Job Tick()
     {
-        drawList.Clear();
-
-        if (!Settings.Enable || !GameController.InGame || areaPumpStopped)
+        if (!Settings.Enable || !GameController.InGame || AreaPumpStopped)
             return null;
 
-        var Player = GameController?.Player;
-        if (Player == null)
-            return null;
+        LargeMapOpen = GameController.Game.IngameState.IngameUi.Map.LargeMap.IsVisible;
+        BlightPathways = PathwayEntityList.OrderByDescending(item => item.Id).ToList();
 
-        _playerGridPos = Player.GetComponent<Positioned>().WorldPosNum.WorldToGrid();
-        var ingameUi = GameController.Game.IngameState.IngameUi;
-        var map = ingameUi.Map;
-        var largeMap = map.LargeMap.AsObject<SubMap>();
-        _largeMapOpen = largeMap.IsVisible;
-        _mapScale = GameController.IngameState.Camera.Height / 677f * largeMap.Zoom;
-        _mapCenter = largeMap.GetClientRect().TopLeft.ToVector2Num() + largeMap.ShiftNum + largeMap.DefaultShiftNum;
-        _playerZ = Player.GetComponent<Render>().Z;
-        var sortedList = BlightEntities.OrderByDescending(item => item.Id).ToList();
-        drawList = sortedList;
-        IngameData = GameController.IngameState.Data;
-
-        PlayerPos = IngameData.ToWorldWithTerrainHeight(Player.PosNum);
-
-        // Check if pump running or not
-        if (areaPumpEntity != null)
-        {
-            areaPumpEntity.TryGetComponent<StateMachine>(out var stateComponent);
-
-            if (stateComponent != null)
-            {
-                foreach (var state in stateComponent.States)
-                    if (state is { Name: "success" or "fail", Value: 1 })
-                        areaPumpStopped = true;
-            }
-        }
+        if (AreaPumpEntity != null &&
+            AreaPumpEntity.TryGetComponent<StateMachine>(out var stateComponent) &&
+            stateComponent.States.Any(state => state.Name is "success" or "fail" && state.Value == 1))
+            AreaPumpStopped = true;
 
         return null;
     }
 
     public override void Render()
     {
-        if (!Settings.Enable || areaPumpStopped || !GameController.InGame || IngameData == null || PlayerPos == Vector3.Zero)
+        if (!Settings.Enable || AreaPumpStopped || !GameController.InGame || GameController.IngameState.Data == null)
             return;
 
         var inGameUi = GameController.Game.IngameState.IngameUi;
@@ -111,143 +70,78 @@ public class Main : BaseSettingsPlugin<BlightSettings>
         if (!Settings.IgnoreLargePanels && inGameUi.LargePanels.Any(x => x.IsVisible))
             return;
 
-        DrawLines(drawList, Settings.BlightWidthMap, Settings.BlightColorMap, Settings.BlightWidthWorld, Settings.BlightColorWorld);
-        DrawTowers();
+        DrawBlightPaths(BlightPathways);
+        DrawTowers(TowerEntityList);
     }
 
-    private void DrawTowers()
+    private void DrawTowers(List<Entity> towerList)
     {
         foreach (var tower in towerList)
         {
-            tower.TryGetComponent<BlightTower>(out var blightTowerComp);
-
-            if (blightTowerComp == null)
+            if (!tower.TryGetComponent<BlightTower>(out var blightTowerComp))
                 continue;
 
             var radius = GetTowerRadius(blightTowerComp.Id);
-
             if (radius == -1)
                 continue;
 
-            var worldRadius = radius / PoeMapExtension.WorldToGridConversion;
-            var entityPos = tower.PosNum;
-            var entityPosScreen = RemoteMemoryObject.pTheGame.IngameState.Camera.WorldToScreen(entityPos);
-            var thickness = Settings.CircleThickness;
-
-            if (IsEntityWithinScreen(entityPosScreen, GetScreenSize(), 200))
-            {
-                switch (blightTowerComp.Id)
-                {
-                    // Fire Towers
-                    case "FlameTower1":
-                    case "FlameTower2":
-                    case "FlameTower3":
-                    case "MeteorTower":
-                    case "FlamethrowerTower":
-                        if (Settings.FireTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.FireTowerColor, thickness, 40);
-
-                        break;
-
-                    // Cold Towers
-                    case "ChillingTower1":
-                    case "ChillingTower2":
-                    case "ChillingTower3":
-                    case "FreezingTower":
-                    case "IcePrisonTower":
-                        if (Settings.ColdTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.ColdTowerColor, thickness, 40);
-
-                        break;
-
-                    // Lightning Towers
-                    case "ShockingTower1":
-                    case "ShockingTower2":
-                    case "ShockingTower3":
-                    case "LightningStormTower":
-                    case "ArcingTower":
-                        if (Settings.LightningTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.LightningTowerColor, thickness, 40);
-
-                        break;
-
-                    // Physical Towers
-                    case "StunningTower1":
-                    case "StunningTower2":
-                    case "StunningTower3":
-                    case "TemporalTower":
-                    case "PetrificationTower":
-                        if (Settings.PhysicalTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.PhysicalTowerColor, thickness, 40);
-
-                        break;
-
-                    // Minion Towers
-                    case "MinionTower1":
-                    case "MinionTower2":
-                    case "MinionTower3":
-                    case "FlyingMinionTower":
-                    case "TankyMinionTower":
-                        if (Settings.MinionTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.MinionTowerColor, thickness, 40);
-
-                        break;
-
-                    // Buff Towers
-                    case "BuffTower1":
-                    case "BuffTower2":
-                    case "BuffTower3":
-                    case "BuffPlayersTower":
-                    case "WeakenEnemiesTower":
-                        if (Settings.BuffTowerGround)
-                            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, Settings.BuffTowerColor, thickness, 40);
-
-                        break;
-                    default:
-                        LogMessage($"Unconfirmed Tower: {blightTowerComp.Id} with radius: {radius}");
-                        break;
-                }
-            }
+            DrawTower(tower, blightTowerComp.Id, radius);
         }
+    }
+
+    private void DrawTower(Entity tower, string towerId, int radius)
+    {
+        var worldRadius = radius / PoeMapExtension.WorldToGridConversion;
+        var isWithinScreen = IsEntityWithinScreen(tower.PosNum, GetScreenSize(), 200);
+
+        var towerSetting = towerId switch
+        {
+            "FlameTower1" or "FlameTower2" or "FlameTower3" or "MeteorTower" or "FlamethrowerTower" => Settings.Towers.FireTower,
+
+            "ChillingTower1" or "ChillingTower2" or "ChillingTower3" or "FreezingTower" or "IcePrisonTower" => Settings.Towers.ColdTower,
+
+            "ShockingTower1" or "ShockingTower2" or "ShockingTower3" or "LightningStormTower" or "ArcingTower" => Settings.Towers.LightningTower,
+
+            "StunningTower1" or "StunningTower2" or "StunningTower3" or "TemporalTower" or "PetrificationTower" => Settings.Towers.PhysicalTower,
+
+            "MinionTower1" or "MinionTower2" or "MinionTower3" or "FlyingMinionTower" or "TankyMinionTower" => Settings.Towers.MinionTower,
+
+            "BuffTower1" or "BuffTower2" or "BuffTower3" or "BuffPlayersTower" or "WeakenEnemiesTower" => Settings.Towers.BuffTower,
+
+            _ => null
+        };
+
+        if (towerSetting == null)
+            return;
+
+        if (towerSetting.DrawGround && isWithinScreen)
+            Graphics.DrawCircleInWorld(tower.PosNum, worldRadius, towerSetting.Color, towerSetting.CircleThickness, 40);
+
+        if (towerSetting.DrawMap && LargeMapOpen)
+            Graphics.DrawCircleOnLargeMap(tower.GridPosNum, true, radius, towerSetting.Color, towerSetting.CircleThickness / 4, 40);
     }
 
     public override void EntityAdded(Entity entity)
     {
         if (entity.Metadata == "Metadata/Terrain/Leagues/Blight/Objects/BlightPathway")
-            BlightEntities.Add(entity);
+            PathwayEntityList.Add(entity);
 
-        entity.TryGetComponent<BlightTower>(out var blightComp);
-
-        if (blightComp != null)
-            towerList.Add(entity);
+        if (entity.TryGetComponent<BlightTower>(out var blightComp))
+            TowerEntityList.Add(entity);
 
         if (entity.Metadata == "Metadata/Terrain/Leagues/Blight/Objects/BlightPump")
-            areaPumpEntity = entity;
+            AreaPumpEntity = entity;
     }
 
     public override void EntityRemoved(Entity entity)
     {
-        var entityToRemove = towerList.FirstOrDefault(tower => tower.Id == entity.Id);
+        var entityToRemove = TowerEntityList.FirstOrDefault(tower => tower.Id == entity.Id);
 
         if (entityToRemove != null)
-            towerList.Remove(entityToRemove);
+            TowerEntityList.Remove(entityToRemove);
     }
 
-    private Vector3 ExpandWithTerrainHeight(Vector2 gridPosition) =>
-        new(gridPosition.GridToWorld(), GameController.IngameState.Data.GetTerrainHeightAt(gridPosition));
-
-    private Vector2 GetWorldScreenPosition(Vector2 gridPos) => Camera.WorldToScreen(ExpandWithTerrainHeight(gridPos));
-
-    private Vector2 GetMapScreenPosition(Vector2 gridPos) =>
-        _mapCenter + TranslateGridDeltaToMapDelta(gridPos - _playerGridPos, GameController.IngameState.Data.GetTerrainHeightAt(gridPos) - _playerZ);
-
-    private Vector2 TranslateGridDeltaToMapDelta(Vector2 delta, float deltaZ)
-    {
-        deltaZ /= GridToWorldMultiplier; //z is normally "world" units, translate to grid
-        return (float)_mapScale * new Vector2((delta.X - delta.Y) * CameraAngleCos, (deltaZ - (delta.X + delta.Y)) * CameraAngleSin);
-    }
-
-    public void DrawLines(List<Entity> drawingOrder, float lineWidthMap, Color lineColorMap, float lineWidthWorld, Color lineColorWorld)
+    public void DrawBlightPaths(List<Entity> drawingOrder)
     {
         if (drawingOrder == null || drawingOrder.Count < 2)
             return;
@@ -260,47 +154,35 @@ public class Main : BaseSettingsPlugin<BlightSettings>
             if (entity1.Id - entity2.Id > 1 || entity1.Distance(entity2) > 35)
                 continue;
 
-            if (Settings.DrawMap && _largeMapOpen)
-                Graphics.DrawLine(GetMapScreenPosition(entity1.GridPosNum), GetMapScreenPosition(entity2.GridPosNum), lineWidthMap, lineColorMap);
+            if (Settings.Pathways.DrawMap && LargeMapOpen)
+                Graphics.DrawLineOnLargeMap(entity1.GridPosNum, entity2.GridPosNum, Settings.Pathways.MapLineWidth, Settings.Pathways.MapColor);
 
-            if (Settings.DrawWorld && _playerGridPos.Distance(entity1.GridPosNum) < Settings.MaxWorldDrawDistance &&
-                _playerGridPos.Distance(entity2.GridPosNum) < Settings.MaxWorldDrawDistance)
-            {
-                Graphics.DrawLine(
-                    GetWorldScreenPosition(entity1.GridPosNum),
-                    GetWorldScreenPosition(entity2.GridPosNum),
-                    lineWidthWorld,
-                    lineColorWorld
-                );
-                //Graphics.DrawText(entity1.Id.ToString(), GetWorldScreenPosition(entity1.GridPosNum));
-            }
+            if (Settings.Pathways.DrawWorld && IsEntityWithinScreen(entity1.PosNum, GetScreenSize(), 200))
+                Graphics.DrawLineInWorld(entity1.GridPosNum, entity2.GridPosNum, Settings.Pathways.WorldLineWidth, Settings.Pathways.WorldColor);
         }
     }
 
-    public RectangleF GetScreenSize() =>
-        new()
-        {
-            X = 0, Y = 0, Width = GameController.Window.GetWindowRectangleTimeCache.Size.Width,
-            Height = GameController.Window.GetWindowRectangleTimeCache.Size.Height
-        };
+    public RectangleF GetScreenSize()
+    {
+        var size = GameController.Window.GetWindowRectangleTimeCache.Size;
+        return new RectangleF { X = 0, Y = 0, Width = size.Width, Height = size.Height };
+    }
 
     public int GetTowerRadius(string blightTowerId)
     {
-        var blightData = GameController.Game.Files.BlightTowers.EntriesList;
-
-        foreach (var record in blightData.Where(record => record.Id == blightTowerId))
-            return record.Radius;
-
-        return -1;
+        var record = GameController.Game.Files.BlightTowers.EntriesList.FirstOrDefault(r => r.Id == blightTowerId);
+        return record?.Radius ?? -1;
     }
 
-    private static bool IsEntityWithinScreen(Vector2 entityPos, RectangleF screenSize, float allowancePX)
+    private static bool IsEntityWithinScreen(Vector3 entityPos, RectangleF screenSize, float allowancePx)
     {
-        // Check if the entity position is within the screen bounds with allowance
-        var leftBound = screenSize.Left - allowancePX;
-        var rightBound = screenSize.Right + allowancePX;
-        var topBound = screenSize.Top - allowancePX;
-        var bottomBound = screenSize.Bottom + allowancePX;
-        return entityPos.X >= leftBound && entityPos.X <= rightBound && entityPos.Y >= topBound && entityPos.Y <= bottomBound;
+        var entityScreenPos = RemoteMemoryObject.pTheGame.IngameState.Camera.WorldToScreen(entityPos);
+
+        var leftBound = screenSize.Left - allowancePx;
+        var rightBound = screenSize.Right + allowancePx;
+        var topBound = screenSize.Top - allowancePx;
+        var bottomBound = screenSize.Bottom + allowancePx;
+
+        return entityScreenPos.X >= leftBound && entityScreenPos.X <= rightBound && entityScreenPos.Y >= topBound && entityScreenPos.Y <= bottomBound;
     }
 }
